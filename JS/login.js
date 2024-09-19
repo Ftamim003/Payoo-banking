ba1f44972e2885e0bda326972aa89ca624c7df69
ftamim003 <fazlullahtamim@gmail.com>
2024-09-19 19:34:42 +0600

    add money and remove section done and a number added for validation

---
+++ JS/login.js
@@ -0,0 +1,18 @@
+document.getElementById('button-login').addEventListener('click',function(event){
+    
+    event.preventDefault();
+
+    // console.log("button clicked")
+
+    const phoneNumber= document.getElementById('phone-number').value;
+    
+   const loginNumber= document.getElementById('login-id').value;
+
+   if(phoneNumber==='01791430459'||phoneNumber==='01845750728' && loginNumber==='1234' ){
+    console.log(" You're logged in");
+    window.location.href="homePage.html";
+   }
+   else{
+    alert("Wrong phone number or pin")
+   }
+})
