bf8791353f4a55d4a37b8b2ad578d6d32132ac0e
ftamim003 <fazlullahtamim@gmail.com>
2024-09-19 13:34:50 +0600

    added amount and pin input partially

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,11 @@
-console.log('Home added');
+
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+
+    event.preventDefault();
+
+    const inputAmount=document.getElementById('input-amount').value;
+
+    const inputPin=document.getElementById('input-pin').value;
+
+    console.log(inputAmount, inputPin);
+})
