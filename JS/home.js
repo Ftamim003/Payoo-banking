ace01b20bb9fa04bc3589cd3ed5a13296727c141
ftamim003 <fazlullahtamim@gmail.com>
2024-09-19 16:43:27 +0600

    error found in homepage

--- JS/home.js
+++ JS/home.js
@@ -7,5 +7,17 @@ document.getElementById('add-money-btn').addEventListener('click',function(event
 
     const inputPin=document.getElementById('input-pin').value;
 
-    console.log(inputAmount, inputPin);
-})
+
+    if(inputPin==='1234'){
+        
+        const balance= document.getElementById('main-balance').innerText;
+
+        const totalBalance= parseInt(inputAmount) + parseInt(balance);
+
+        document.getElementById('main-balance').innerText=totalBalance;
+    }
+    else{
+      alert("Invalid input! Please type valid input")  
+    }
+
+}) 
